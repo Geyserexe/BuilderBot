ead101444da64df373e97cb83d61c862bad0ddde
Geyserexe <ben.lenger@googlemail.com>
2020-07-30 17:58:31 -0600

    still not working

--- builder.js
+++ builder.js
@@ -33,11 +33,13 @@ function makeRequest(){
         const http = require('http');
         const stream = require('stream');
         const options = {
+            authority: 'pokepast.es',
             hostname: 'pokepast.es',
             port: 80,
             path: '/create',
             method: 'POST', 
-            body: {name: "BuilderBotExport",teamString, author: "Geysers' BuilderBot", notes: `cutoff:${config.cutoff}\nbuilder:${config.builder}\ntier:${config.tier}`}
+            accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            body: {name: "BuilderBotExport",teamString, author: "Geysers' BuilderBot", notes: `cutoff:${config.cutoff}\nbuilder:${config.builder}\ntier:${config.tier}\nhttps://github.com/Geyserexe/BuilderBot`},
         };
 
 
@@ -62,8 +64,9 @@ async function exportTeam(){
     console.log("exporting");
     const result = await makeRequest();
     console.log(result);
-    console.log(result.read());
+    console.log(Object.entries(result));
     console.log(Object.keys(result));
+    console.log(result.headers.status);
     console.log("test");
 }
 
