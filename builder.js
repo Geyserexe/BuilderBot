d721cfef83673cd259d8ab478ab808fd90b943e5
Geyserexe <ben.lenger@googlemail.com>
2020-08-02 20:35:20 -0600

    fixed lingering optimizer remnants & added sets

--- builder.js
+++ builder.js
@@ -28,9 +28,8 @@ const config = require("./config.json");
 
 let teamString = "";
 
-if (config.mode.toLowerCase() === "optimize") {
-    teamString = require("./src/optimizer.js");
-} else if (config.mode.toLowerCase() != "offense") {
+
+if (config.mode.toLowerCase() != "offense") {
     teamString = require(`./src/${config.gen}${config.mode.toLowerCase()}builder.js`);
 } else {
     teamString = require(`./src/offensebuilder.js`);
