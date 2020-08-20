02e8c685c6160d59c808bbd962cc9b74638397a7
Geyserexe <ben.lenger@googlemail.com>
2020-08-19 18:14:14 -0600

    1.0.11

--- builder.js
+++ builder.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
 const config = require("./config.js");
-const upload = require('./src/uploadtopokepaste.js');
+const { uploadToPokepaste } = require('./src/uploadtopokepaste.js');
 
 let team = "";
-
-console.log("building...");
 
 team = require(`./src/builders/${config.tier}/${config.mode}builder.js`);
 
@@ -13,8 +11,7 @@ async function exportTeam() {
     if(team.includes("error")){
         console.log(team);
     } else {
-        console.log("exporting..."); 
-        console.log(await upload.uploadToPokepaste(team));
+        console.log(await uploadToPokepaste(team));
     }
 }
 
