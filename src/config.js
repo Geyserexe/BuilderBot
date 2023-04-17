5e9c121918486ac2262d94670fc73e78f7f078df
Geyserexe <ben.lenger@googlemail.com>
2023-04-17 17:11:23 -0500

    fix broken -raw and revamp flags

--- src/config.js
+++ src/config.js
@@ -9,50 +9,55 @@ for (let i = 0; i < process.argv.length; i += 2) {
     let flag = process.argv[i];
     switch (flag) {
         case 'help':
-            throw ('usage: node builder [--c (arg #)| --n (arg #)| --t (arg tier)| --r (arg #)| --b (arg #)| --m (arg mode)| --cm | --d | --raw | --a]');
-        case '--c':
+            throw ('usage: node builder [-c (arg #)| -n (arg #)| -t (arg tier)| -r (arg #)| -b (arg #)| -m (arg mode)| -cm | -d | -raw | -a]');
+        case '-c':
             if (parseInt(value) >= 10) {
                 throw ('cutoff too high (lower it below 10)');
             }
             configEdits.cutoff = parseInt(value);
             break;
-        case '--t':
-            if(!(value == "gen8anythinggoes" || value == "gen7anythinggoes")){
+        case '-t':
+            if(!(value.toLowerCase() == "gen8anythinggoes" || value.toLowerCase() == "gen7anythinggoes")){
                 throw("invalid tier");
             }
-            tier = value;
+            tier = value.toLowerCase();
             break;
-        case '--r':
+        case '-r':
             configEdits.recurseThreshold = parseInt(value);
             break;
-        case '--b':
+        case '-b':
             configEdits.breakerThreshold = parseInt(value);
             break;
-        case '--m':
-            if(!(value == "balance" || value == "offense")){
+        case '-m':
+            if(!(value.toLowerCase() == "balance" || value.toLowerCase() == "offense")){
                 throw("invalid mode");
             }
-            mode = value;
+            mode = value.toLowerCase();
             break;
-        case '--a':
+        case '-a':
             configEdits.monsToAvoid=[];
             for (let mon of value.split(',')) {
-                configEdits.monsToAvoid.push(mon);
+                configEdits.monsToAvoid.push(mon.toLowerCase());
             }
             break;
-        case '--n':
+        case '-n':
+            if(value > 2000){
+                throw("team number too large");
+            }
             configEdits.teamNumber = parseInt(value);
             break;
-        case '--d':
+        case '-d':
             configEdits = defaultconfig;
             i = process.argv.length;
             break;
-        case '--raw':
+        case '-raw':
             configEdits.raw = true;
             i--;
-        case '--nsc':
+            break;
+        case '-nsc':
             configEdits.speciesClause = false;
             i--;
+            break;
     }
 }
 let config = null;
@@ -60,7 +65,6 @@ let config = null;
 if (!tier) { tier = "gen8anythinggoes"; }
 if (!mode) { mode = "balance" }
 config = configTemplates[tier][mode];
-
 for (let [key, value] of Object.entries(configEdits)) {
     for (let [key1, value1] of Object.entries(config)) {
         if (key == key1 && value != value1) {
